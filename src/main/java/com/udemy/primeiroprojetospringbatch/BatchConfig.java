e45a8343571b9b39d6443b9a599c0e40fd25035d
Anderson Santos <andersonluizsantos@gmail.com>
2021-12-29 10:58:32 -0300

    Múltiplas execuções e obtento valores dos argumentos

--- src/main/java/com/udemy/primeiroprojetospringbatch/BatchConfig.java
+++ src/main/java/com/udemy/primeiroprojetospringbatch/BatchConfig.java
@@ -6,10 +6,13 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,19 +28,29 @@ public class BatchConfig {
 	
 	@Bean
 	public Job imprimeJob() {
-		return jobBuilderFactory.get("imprimeOlaJob").start(imprimeOlaStep()).build();
+		return jobBuilderFactory
+				.get("imprimeOlaJob")
+				.start(imprimeOlaStep())
+				.incrementer(new RunIdIncrementer())
+				.build();
 	}
 
 	private Step imprimeOlaStep() {
 		
-		return stepBuilderFactory.get("imprimeOlaStep").tasklet(new Tasklet() {
+		return stepBuilderFactory.get("imprimeOlaStep").tasklet(imprimeOlaTasklet(null)).build();
+	}
+
+	@Bean
+	@StepScope
+	public Tasklet imprimeOlaTasklet(@Value("#{jobParameters['nome']}") String nome) {
+		return new Tasklet() {
 			
 			@Override
 			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-				System.out.println("Olá MUndo");
+				System.out.println("Olá " + nome);
 				return RepeatStatus.FINISHED;
 			}
-		}).build();
+		};
 	}
 	
 	
